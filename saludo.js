e3661a4dd670dd267ea097f980700cf7c4719bfa
aldair <aldairgarciap15@gmail.com>
2025-10-01 09:20:24 +0200

    añadir mensaje de motivación

--- saludo.js
+++ saludo.js
@@ -2,4 +2,7 @@ const nombre = "Aldair";
 const apellido1 = "García";
 const apellido2 = "Pérez";
 
+const modificacion = "Voy a aprobar el módulo de DWEC";
+
 console.log(nombre, apellido1, apellido2);
+console.log(modificacion);
