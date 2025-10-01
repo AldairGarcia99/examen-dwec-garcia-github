2405d8d9b147bfc5b7bb82e5d76827683ca3d1ef
aldair <aldairgarciap15@gmail.com>
2025-10-01 09:55:06 +0200

    nueva modificacion

--- saludo.js
+++ saludo.js
@@ -2,7 +2,8 @@ const nombre = "Aldair";
 const apellido1 = "García";
 const apellido2 = "Pérez";
 
-const modificacion = "Voy a aprobar el módulo de DWEC";
+const modificacion = "Voy a aprobar el módulo de DWEC ";
 
+console.log(nombre, apellido1, apellido2);
 console.log(nombre, apellido1, apellido2);
 console.log(modificacion);
